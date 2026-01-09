d405b9914cec088c81febc8d25aa9fc19296ccd8
smarthubservice-admin <smarthubserviceinfo@gmail.com>
2026-01-09 15:25:52 +0000

    gh-pages config

--- server.js
+++
@@ -1,4 +0,0 @@
-// server.js has been removed per project simplification.
-// The original server implementation was saved to `scripts/old_server.js`.
-// If you need to run a backend, restore that file to project root as `server.js`.
-console.log('server.js is intentionally disabled in this branch.');
